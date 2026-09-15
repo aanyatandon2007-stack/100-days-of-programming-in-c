30505523d9f4adf000c1ce9a15cbd7a5bcab447c
AanyaTandon <aanyatandon2007@gmail.com>
2026-09-15 17:59:58 +0000

    Add division by zero handling program

---
+++ Day1/division.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b;
+
+    printf("Enter two numbers: ");
+    scanf("%f %f", &a, &b);
+
+    if (b == 0)
+    {
+        printf("Division by zero is not allowed.\n");
+    }
+    else
+    {
+        printf("Quotient = %.2f\n", a / b);
+    }
+
+    return 0;
+}
